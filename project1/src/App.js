210e77b4d5f7c71b057077aab598d48445621fa6
NCR Local Administrator <ncrlocaladministrator@NCRs-MacBook-Pro.local>
2023-03-27 14:34:35 +0530

    structure is created

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,23 +1,21 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Component/Navbar'
+import {Routes,Route} from 'react-router-dom';
+import Home from './Pages/Home';
+import Qualification from './Pages/Qualification';
+import Skills from './Pages/Skills';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Navbar/>
+        <Routes>
+        <Route path="/Home" element={<Home/>}/>
+        <Route path="/Qualification" element={<Qualification/>}/>
+        <Route path="/Skills" element={<Skills/>}/>
+</Routes>
     </div>
   );
 }
